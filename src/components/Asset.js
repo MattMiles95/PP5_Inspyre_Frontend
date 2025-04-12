fbaae548c74c1fa8aeb113d0146f48af6e1e2f5f
Matt Miles <matt_miles2@live.co.uk>
2025-04-12 15:32:40 +0100

    Add Asset component.

---
+++ src/components/Asset.js
@@ -0,0 +1,20 @@
+// React
+import React from "react";
+
+// Bootstrap Components
+import Spinner from "react-bootstrap/Spinner";
+
+// CSS
+import styles from "../styles/Asset.module.css";
+
+const Asset = ({ spinner, src, message }) => {
+  return (
+    <div className={`${styles.Asset} p-4`}>
+      {spinner && <Spinner animation="border" />}
+      {src && <img src={src} alt={message} />}
+      {message && <p className="mt-4">{message}</p>}
+    </div>
+  );
+};
+
+export default Asset;
