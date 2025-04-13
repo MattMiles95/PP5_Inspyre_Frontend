12f3ec254ae6ba9a8f02caa5ffcbaeface549d57
Matt Miles <matt_miles2@live.co.uk>
2025-04-13 17:45:46 +0100

    Add FetchMoreData to utils.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,6 +4,29 @@ import { axiosReq } from "../api/axiosDefaults";
 // Auth Token
 import { jwtDecode } from "jwt-decode";
 
+export const fetchMoreData = async (resource, setResource) => {
+  try {
+    const { data } = await axiosReq.get(resource.next);
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur];
+      }, prevResource.results),
+    }));
+  } catch (err) {
+    // console.error("Error fetching more data:", err);
+    if (err.response?.status !== 401) {
+      setResource((prevResource) => ({
+        ...prevResource,
+        next: null,
+      }));
+    }
+  }
+};
+
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
   localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
