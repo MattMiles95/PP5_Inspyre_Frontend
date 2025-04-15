9613ba49fbacd286b9d5f663612421bbc6794cc2
Matt Miles <matt_miles2@live.co.uk>
2025-04-15 12:27:34 +0100

    Add css to PostDropdown.

--- src/components/PostDropdown.js
+++ src/components/PostDropdown.js
@@ -7,20 +7,35 @@ import Dropdown from "react-bootstrap/Dropdown";
 // Bootstrap Icons
 import { ThreeDots } from "react-bootstrap-icons";
 
+// CSS
+import styles from "../styles/PostDropdown.module.css";
+
 const PostDropdown = ({ handleEdit, handleDelete, handleReport }) => {
   return (
     <Dropdown>
-      <Dropdown.Toggle variant="link" bsPrefix="p-0">
+      <Dropdown.Toggle
+        variant="link"
+        bsPrefix={styles.DropdownToggle}
+        aria-label="Post options"
+      >
         <ThreeDots />
       </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        {handleEdit && <Dropdown.Item onClick={handleEdit}>Edit</Dropdown.Item>}
+      <Dropdown.Menu className={styles.DropdownMenu}>
+        {handleEdit && (
+          <Dropdown.Item className={styles.DropdownItem} onClick={handleEdit}>
+            Edit
+          </Dropdown.Item>
+        )}
         {handleDelete && (
-          <Dropdown.Item onClick={handleDelete}>Delete</Dropdown.Item>
+          <Dropdown.Item className={styles.DropdownItem} onClick={handleDelete}>
+            Delete
+          </Dropdown.Item>
         )}
         {handleReport && (
-          <Dropdown.Item onClick={handleReport}>Report</Dropdown.Item>
+          <Dropdown.Item className={styles.DropdownItem} onClick={handleReport}>
+            Report
+          </Dropdown.Item>
         )}
       </Dropdown.Menu>
     </Dropdown>
