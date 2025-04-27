915523df764999b2d9b9cdc7a81d61aaaa0a4301
Matt Miles <matt_miles2@live.co.uk>
2025-04-27 21:12:53 +0100

    Add Modal component and associated css.

---
+++ src/components/Modal.js
@@ -0,0 +1,40 @@
+// React
+import React from "react";
+
+// CSS
+import styles from "../styles/Modal.module.css";
+
+const Modal = ({ isOpen, onClose, title, children, onConfirm }) => {
+  if (!isOpen) return null;
+
+  return (
+    // Modal Overlay
+    <div className={styles.ModalOverlay}>
+      <div className={styles.ModalContent}>
+        <div className={styles.ModalHeader}>
+          <h4 className={styles.ModalTitle}>{title}</h4>
+          <button onClick={onClose} className={styles.CloseButton}>
+            x
+          </button>
+        </div>
+
+        {/* Modal Body */}
+        <div className={styles.ModalBody}>{children}</div>
+        
+        {/* Modal Footer */}
+        <div className={styles.ModalFooter}>
+          <button onClick={onClose} className={styles.CancelButton}>
+            Cancel
+          </button>
+          {onConfirm && (
+            <button onClick={onConfirm} className={styles.DeleteButton}>
+              Delete
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
